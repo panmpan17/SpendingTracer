5db635c1678ab7a69b626b219a5d9c042f1af409
panmpan17 <panmpan@gmail.com>
2017-09-25 16:16:52 +0800

    Initial Commit

---
+++ Easy Record/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Easy Record
+//
+//  Created by Michael Pan on 9/25/17Monday.
+//  Copyright © 2017 Michael Pan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
